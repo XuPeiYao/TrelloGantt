4b092d5d72b61f24a97f3b6c296ea6dd9fadbd61
XuPeiYao <st89382000@gmail.com>
2018-11-04 22:31:50 +0800

    加入卡片按鈕測試

--- src/app/card/card-routing.module.ts
+++ src/app/card/card-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DependenciesComponent } from './components/dependencies/dependencies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DependenciesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CardRoutingModule { }
+export class CardRoutingModule {}
