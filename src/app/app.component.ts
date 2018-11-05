881444f736d32f99dc03d1a2c570ef2ff07ae1a5
XuPeiYao <st89382000@gmail.com>
2018-11-05 11:08:27 +0800

    實作卡片LINE分享功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,9 +39,7 @@ export class AppComponent implements OnInit {
               icon: 'https://social-plugins.line.me/img/web/lineit_select_line_icon_03.png',
               text: '用 LINE 傳送',
               callback: (t: any) => {
-                window['t'] = t;
-                console.log(t.getContext());
-                const url = '';
+                const url = `https://trello.com/c/${t.getContext().card}`;
                 window.open(`https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(url)}`, null, 'width=503,height=500,resizable=yes,scrollbars=no,chrome=yes,centerscreen=yes');
               }
             },
